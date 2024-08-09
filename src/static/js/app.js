8ebe85ec294b7264de81ec42b76abc71d9daf66f
Katerina Zakharova <kzakharova96@yandex.com>
2024-08-09 16:56:06 +0300

    worked with DBs, volume and bind mounts in Docker

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding new plan...' : 'Add item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
